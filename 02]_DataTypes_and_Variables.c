cdfb4c60db5b4eb91c0531e9a4ab4dde88bb09ca
Raj Rasane <rasaneraj04@gmail.com>
2024-06-20 23:46:31 +0530

    new notes added

---
+++ 02]_DataTypes_and_Variables.c
@@ -0,0 +1,49 @@
+// Basic Data Types:
+
+// - int: Integer (e.g., 10, -5)
+    /*
+        Size: 4 bytes (32 bits) on most systems.
+        Range: -2,147,483,648 to 2,147,483,647.
+        Format Specifier: %d.
+        Used for whole numbers (positive, negative, or zero).
+    */
+
+
+// - float: Floating-point number (e.g., 3.14, -2.5)
+    /*
+        Size: 4 bytes (32 bits).
+        Range: Approximately ±3.4 × 10^-38 to ±3.4 × 10^38.
+        Format Specifier: %f.
+        Used for single-precision floating-point numbers.
+    */
+
+// - double: Double precision floating-point number (e.g., 3.14159)
+    /*
+        Size: 8 bytes (64 bits).
+        Range: Approximately ±1.7 × 10^-308 to ±1.7 × 10^308.
+        Format Specifier: %lf.
+        Used for double-precision floating-point numbers (more precise than float).
+    */
+
+// - char: Character (e.g., 'a', '1')
+    /*
+        Size: 1 byte (8 bits).
+        Range: Typically represents a single character (e.g., ‘a’, ‘1’).
+        Format Specifier: %c.
+        Used for storing individual characters.
+    */
+
+//---------------------------------------------------------------------------------------------------------------------------------------
+
+
+// Variable Declaration and Initialization:
+
+int a = 10;
+float b = 3.14;
+char c = 'A';
+
+// Constants and Literals:
+
+#define PI 3.14 // Macro definition
+const int MAX = 100; // Constant variable
+
