11fbc49235f75400324ebee81088424f88b0b6a7
Raj Rasane <rasaneraj04@gmail.com>
2024-06-20 23:40:23 +0530

    new notes added

---
+++ 01]_Introduction.c
@@ -0,0 +1,13 @@
+// Basic Structure of a C Program:
+
+#include <stdio.h> // Preprocessor directive
+
+int main() { // Main function
+    printf("Hello, World!\n"); // Output statement
+    return 0; // Return statement
+}
+
+// - #include <stdio.h>: Includes the Standard Input Output header file.
+// - int main(): Entry point of the program.
+// - printf(): Function to print text to the console.
+// - return 0;: Indicates successful program termination.
