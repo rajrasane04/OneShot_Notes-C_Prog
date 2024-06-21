470ecf498d744fe932c4f96e56e98577c38b6850
Raj Rasane <rasaneraj04@gmail.com>
2024-06-21 19:27:53 +0530

    new notes added

---
+++ 03]_Operators_and_Expressions.c
@@ -0,0 +1,33 @@
+// Arithmetic Operators:
+
+// Addition (+) :-   Adds two operands.
+// Subtraction (-) :-  Subtracts the second operand from the first.
+// Multiplication (*) :-  Multiplies two operands.
+// Division (/) :-  Divides the first operand by the second.
+// Modulus (%) :-  Returns the remainder after division.
+
+//---------------------------------------------------------------------------------------------------------------------------------------
+
+// Relational Operators:
+
+// Equal to (==) :-  Checks if two values are equal.
+// Not equal to (!=) :-  Checks if two values are not equal.
+// Greater than (>) :-  Checks if the first value is greater than the second.
+// Less than (<) :-  Checks if the first value is less than the second.
+// Greater than or equal to (>=) :-  Checks if the first value is greater than or equal to the second.
+// Less than or equal to (<=) :-  Checks if the first value is less than or equal to the second.
+
+//---------------------------------------------------------------------------------------------------------------------------------------
+
+// Logical Operators:
+
+// Logical AND (&&) :-  Returns true if both operands are true.
+// Logical OR (||) :-  Returns true if at least one operand is true.
+// Logical NOT (!) :-  Inverts the value of the operand (true becomes false, and vice versa).
+
+//---------------------------------------------------------------------------------------------------------------------------------------
+
+// Assignment Operators:
+
+// Assignment (=) :-  Assigns the value of the right operand to the left operand.
+// Compound assignment (+=, -=, *=, /=, %=) :-  Performs an operation and assigns the result to the left operand (e.g., a += 5 is equivalent to a = a + 5).
