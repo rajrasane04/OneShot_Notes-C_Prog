2ec90b861735ecf128054e09611d4856c41f86f5
Raj Rasane <rasaneraj04@gmail.com>
2024-06-26 18:01:27 +0530

    new notes added

---
+++ 06]_Functions.c
@@ -0,0 +1,18 @@
+// Functions 
+
+
+// Function Declaration and Definition :
+
+int add(int a, int b); // Declaration
+
+int main() {
+    int result = add(5, 3);
+    printf("Sum: %d\n", result);
+    return 0;
+}
+
+int add(int a, int b) { // Definition
+    return a + b;
+}
+
+//---------------------------------------------------------------------------------------------------------------------------------------
